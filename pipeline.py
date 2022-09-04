ecfbde21ca871f34d025053c7cf20b31ac44689d
arkiver <arkiver@protonmail.com>
2022-09-05 00:57:45 +0200

    Version 20220904.02. Multi items size 20.

--- pipeline.py
+++ pipeline.py
@@ -59,11 +59,11 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20220904.01'
+VERSION = '20220904.02'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'tjournal'
 TRACKER_HOST = 'legacy-api.arpa.li'
-MULTI_ITEM_SIZE = 1
+MULTI_ITEM_SIZE = 20
 
 
 ###########################################################################
