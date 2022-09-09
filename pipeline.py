e3e06fe4c1cba4a4e4e247ab3925da782ce4cf7d
arkiver <arkiver@protonmail.com>
2022-09-09 22:19:33 +0200

    Version 20220909.01. Use browser user agent.

--- pipeline.py
+++ pipeline.py
@@ -59,8 +59,8 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20220906.01'
-USER_AGENT = 'Archive Team'
+VERSION = '20220909.01'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
 TRACKER_ID = 'tjournal'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 20
